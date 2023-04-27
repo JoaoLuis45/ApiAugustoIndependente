87b71c4596296c7fff80ed5a8c008fee0c7c7448
Joao Luis <joaoluisramos45@gmail.com>
2023-04-27 13:32:41 -0300

    feat: quantity tables

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if sys.platform == "win32":
 
 setup(
     name="Gerador de QRcode",
-    version="0.1.1",
+    version="1.1.0",
     description="gerador de QRcode!",
     options={"build_exe": build_exe_options},
     executables=[Executable("tela.py", base=base)]
--- tela.py
+++ tela.py
@@ -6,10 +6,15 @@ import qrcode
 def CriaPasta():
     if not os.path.isdir("QRcodes"):
         os.mkdir('QRcodes')
+def CriaPastaEspecifica(name):
+    if not os.path.isdir(f"QRcodes/{name}"):
+        os.mkdir(f'QRcodes/{name}')
 
 def TelaLogin():
     sg.theme('LightBrown')
     layout = [
+        [sg.Text('Nome do Comércio')],
+        [sg.Input('',key='localName')],
         [sg.Text('SSID')],
         [sg.Input('',key='ssid')],
         [sg.Text('Senha')],
@@ -18,8 +23,10 @@ def TelaLogin():
         [sg.Input('',key='encryption')],
         [sg.Text('IP')],
         [sg.Input('',key='ip')],
-        [sg.Text('Mesa')],
-        [sg.Input('',key='mesa')],
+        [sg.Text('Mesa Inicial')],
+        [sg.Input('',key='mesaInit')],
+        [sg.Text('Mesa Final')],
+        [sg.Input('',key='mesaFinish')],
         [sg.Button('Mesa',key='confirmComMesa'),sg.Button('Wifi',key='confirmSemMesa')]
     ]
     return sg.Window('Formulário API',layout=layout,finalize=True)
@@ -35,19 +42,37 @@ def ChamarApp():
         if window == janela and event == sg.WINDOW_CLOSED:
             break
         elif window == janela and event == 'confirmComMesa':
-           if values['mesa'] == '' or values['ip'] == '':
-                sg.popup('Preencha os campos mesa e ip para prosseguir!')
+           if values['mesaInit'] == '' or values['ip'] == '' or values['localName'] == '':
+                sg.popup('Preencha os campos Nome do Comércio, mesa Inicial e ip para prosseguir!')
            else:
-                ip = values['ip']
-                mesa = values['mesa']
-                myobj2 = {
-                        'appLink': f'{ip}:5002/mesa',
-                        'mesa': mesa
-                        }
-                mesaString = f'http://{myobj2["appLink"]}/{myobj2["mesa"]}'
                 CriaPasta()
-                ImgQRcode = qrcode.make(mesaString)
-                ImgQRcode.save(f'QRcodes/qrcode_{myobj2["mesa"]}.png')
+                CriaPastaEspecifica(values['localName'])
+                ip = values['ip']
+                
+                if(values['mesaFinish'] == ''):
+                    mesa = values['mesaInit']
+                    myobj2 = {
+                            'appLink': f'{ip}:5002/mesa',
+                            'mesa': mesa
+                            }
+                    mesaString = f'http://{myobj2["appLink"]}/{myobj2["mesa"]}'
+                    ImgQRcode = qrcode.make(mesaString)
+                    ImgQRcode.save(f'QRcodes/{values["localName"]}/qrcode_{values["localName"]}_{myobj2["mesa"]}.png')
+                    sg.popup()
+                else:
+                    mesaFinish = values['mesaFinish']
+                    for i in range(int(values['mesaInit']),int(values['mesaFinish']) + 1):
+                        mesa = i
+                        myobj2 = {
+                                'appLink': f'{ip}:5002/mesa',
+                                'mesa': mesa
+                                }
+                        mesaString = f'http://{myobj2["appLink"]}/{myobj2["mesa"]}'
+                        ImgQRcode = qrcode.make(mesaString)
+                        ImgQRcode.save(f'QRcodes/{values["localName"]}/qrcode_{values["localName"]}_{myobj2["mesa"]}.png')
+                    
+
+               
                 # APIaug.GeraQRcode(x2,mesa)
         elif window == janela and event == 'confirmSemMesa':
             if values['ssid'] == '' or values['password'] == '' or values['encryption'] == '':
